b8b7deedb00d2e34f4d7da166e88ceb6c7ed1971
Don Kittle <don@kittle.ca>
2022-11-29 14:02:41 -0500

    move ResultSet into try-with-resources statement

--- src/main/java/ca/kittle/repositories/EncounterRepository.java
+++ src/main/java/ca/kittle/repositories/EncounterRepository.java
@@ -5,10 +5,7 @@ import ca.kittle.models.app.Encounter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,14 +17,11 @@ public class EncounterRepository {
     public List<Encounter> encounters(/** User user */) {
         logger.info("Get all encounters");
         var result = new ArrayList<Encounter>();
-
         var query = "SELECT e.*, c.campaign_name, l.name AS location FROM encounters e LEFT OUTER JOIN campaigns c ON e.campaign_id = c.id LEFT OUTER JOIN locations l ON e.location_id = l.id WHERE e.created_by=1";
 
         try (Connection connection = DriverManager.getConnection(db.jdbcConnectString(),db.username(),db.password());
              Statement statement = connection.createStatement();
-        ) {
-            logger.info("Connected to Postgres");
-            var rs = statement.executeQuery(query);
+             ResultSet rs = statement.executeQuery(query)) {
             while (rs.next()) {
                 result.add(new Encounter(rs.getLong("id"),
                         rs.getString("name"),
@@ -38,6 +32,7 @@ public class EncounterRepository {
         } catch (SQLException e) {
             logger.error("Problem getting all encounters", e);
         }
+        logger.debug("Returning {} encounters", result.size());
         return result;
     }
 
