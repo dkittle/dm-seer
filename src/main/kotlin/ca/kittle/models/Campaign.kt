a13a4f6b04a083d3847d8e596f76852bee822cea
Don Kittle <don@kittle.ca>
2022-12-10 16:34:09 -0500

    remove unneeded import

--- src/main/kotlin/ca/kittle/models/Campaign.kt
+++ src/main/kotlin/ca/kittle/models/Campaign.kt
@@ -1,7 +1,6 @@
 package ca.kittle.models
 
 import kotlinx.serialization.Serializable
-import java.io.Serial
 
 @Serializable
 data class Campaign(val id: Long, val name: String, val description: String, val publicNotes: String, val privateNotes: String, val official: Boolean)
